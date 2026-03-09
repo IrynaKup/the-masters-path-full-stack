afb6825b9fbaf58650ae7779fb17c90d4807b24f
Iryna Kuptsova <152500274+IrynaKup@users.noreply.github.com>
2026-03-09 12:01:01 +0100

    Create primitive-db.js

---
+++ 02_CLI_Primitive-DB/primitive-db.js
@@ -0,0 +1,27 @@
+import { input, select, confim } from '@inquirer/prompts';
+
+const users = [];
+
+
+/* func1 add users
+
+for ( ; is Adding === true; )
+const name = await imput ({message: 'Enter user name (or press ENTER to finish):',});
+if (name.trim() === ''){
+   console.log('Finish registration.');
+   break;
+}
+const gender = await select({message: 'Select gender for ${name}:',
+  choices: [
+  {name: 'Male', value: 'male'},
+  {name: 'Female', value: 'female'},
+  ],  
+});
+const age = await imput({message: 'Enter age for ${name}:',
+});
+users.push({
+      name: name.trim(),
+      gender: gender,
+      age: parseInt(age, 10),
+    });
+*/
